63a38f27ee9392efbeb171bac971cb88fc66cf83
System Administrator <root@hezhuofandeMacBook-Pro.local>
2017-01-11 17:15:07 +0800

    some

--- src/main/java/com/importsource/cloud/eureka/Application.java
+++ src/main/java/com/importsource/cloud/eureka/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 @RestController
 @EnableAutoConfiguration
+@EnableZuulProxy
 public class Application {
     @RequestMapping("/")
     public String home() {
+       // try {
+      //      Thread.sleep(6000);
+     //   } catch (InterruptedException e) {
+       //     e.printStackTrace();
+      //  }
         return "Hello world";
 
     }
